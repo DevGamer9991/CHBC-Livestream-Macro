a07f05627593538a1049ae346a202d8326feed54
DevGamer9991 <pathornton114@gmail.com>
2020-08-30 10:14:41 -0500

    Update

--- src/main/java/com/parker/App.java
+++ src/main/java/com/parker/App.java
@@ -1,29 +1,9 @@
 package com.parker;
 
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.parker.facebook.GetFacebookData;
-import com.sun.net.httpserver.*;
-import org.slf4j.LoggerFactory;
-
-import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLEngine;
-import javax.net.ssl.SSLParameters;
-import java.io.*;
-import java.net.InetSocketAddress;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.sql.SQLSyntaxErrorException;
-
-import static java.net.HttpURLConnection.HTTP_OK;
 
 public class App
 {
-
   public static final String DevKey = "AIzaSyCuumDlKGiJJdHa5IIkp5FxydAjn7zUAhY";
   // App ID: 186968813458771
 
