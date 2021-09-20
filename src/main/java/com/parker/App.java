26dde6e4c2fd93c9df83992857da603e70ed6e83
DevGamer9991 <pathornton114@gmail.com>
2021-09-19 20:02:50 -0500

    Update

--- src/main/java/com/parker/App.java
+++ src/main/java/com/parker/App.java
@@ -9,6 +9,15 @@ public class App
 
   // ISO 8601 Date and Time Format: 2021-07-31T20:37
 
+  /*
+    THINGS TO ADD:
+
+    • Livestream Preview
+    • Add Togglable Console Logging
+    • Web GUI ( Using Nodejs and/or React )
+
+   */
+
   public static void main(String[] args) throws Exception {
     new GetFacebookData().checkDataFile();
     new HTTPSServer().startServer();
