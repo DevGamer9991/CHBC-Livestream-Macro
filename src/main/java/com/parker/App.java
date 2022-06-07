3b6fa1c2d219dca72493d65332a907a1706aedec
Parker Thornton <admin@example.com>
2022-06-07 02:10:20 +0000

    Update

--- src/main/java/com/parker/App.java
+++ src/main/java/com/parker/App.java
@@ -4,7 +4,7 @@ import com.parker.facebook.GetFacebookData;
 
 public class App
 {
-  public static final String DevKey = "AIzaSyCuumDlKGiJJdHa5IIkp5FxydAjn7zUAhY";
+  public static final String DevKey = "";
   // FB App ID: 186968813458771
 
   // ISO 8601 Date and Time Format: 2021-07-31T20:37
